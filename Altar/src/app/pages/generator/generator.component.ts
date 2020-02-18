2db2cbca2ab3fe44afb1cf478036c85d76e0711b
Daemon <makushin.dima@gmail.com>
2020-02-18 23:09:49 +0200

    up

--- Altar/src/app/pages/generator/generator.component.ts
+++ Altar/src/app/pages/generator/generator.component.ts
@@ -4,120 +4,163 @@ import * as moment from 'moment';
 import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-generator',
-  templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.scss']
+	selector: 'app-generator',
+	templateUrl: './generator.component.html',
+	styleUrls: ['./generator.component.scss']
 })
 export class GeneratorComponent implements OnInit {
-  charter_max: number = 20;
-  coltb = [0,1,2,3,4,5,6,7,8,9];
-  mytime    : any;
-  character : string;
-  valide    : boolean = true;
-  finish_generate = false;
-  generate_arr = [
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'y', 's', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
-  ];
+	charter_max: number = 20;
+	coltb = [0,1,2,3,4,5,6,7,8,9];
+	mytime    : any;
+	character : string;
+	valide: boolean = true;
+	code: string = "11";
+	finish_generate = false;
+	dark = true;
+	fixed = false;
+	generate_arr = [
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'y', 's', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+		['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+	];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-    setInterval(() => { this.mytime = moment().format('H:mm:ss'); }, 1000); 
-    this.finish_generate = true;
-    this.onGenerateGrid();
-  }
+	ngOnInit(): void {
+		setInterval(() => { this.mytime = moment().format('H:mm:ss'); }, 1000); 
+		this.finish_generate = true;
+		this.onGenerateGrid();
+	}
 
-  
-  onGenerateCharacter(){
-    let letters = 'abcdefghijklmnopqrstuvwxyz';
-    let item = Math.floor(Math.random()*letters.length);
-    console.log(letters[item]);
-    return letters[item];
-  }
+	onClassGrid() {
+		if (this.dark !== true) {
+		this.dark = true;
+		return this.dark
+		}
+		this.dark = false;
+		return this.dark
+	}
 
-  onGenerateGrid(){
-    let count_first_character: number = 0;
-    let count_second_character: number = 0;
-    let time = moment().format('ss');
-    let first_character : string = this.generate_arr[time[0]][time[1]];
-    let second_character : string = this.generate_arr[time[1]][time[0]];
-    console.log(`time: "${time}" first_character: ${first_character} second_character: ${second_character}`);
-    this.generate_arr.map(
-      (val, index) => {
-        val.map(
-          (value, indx) =>{
-            if(first_character  === this.generate_arr[index][indx]){
-              count_first_character = count_first_character + 1;
-              if(count_first_character>9){
-                this.generate_arr[index][indx] = this.onGenerateCharacter();
-              }
-              10/9
-1.1111111111111112
-Math.ceil(1.11)
-2
-10/2
-5
-28/Math.ceil(28/9)
-7
-118/Math.ceil(118/9)
-8.428571428571429
-89/Math.ceil(118/9)
-6.357142857142857
-89/7
-12.714285714285714
-            }
-            
-          }
-        )
-      }
-    );
-  }
+	onGenerateCharacter(){
+		let letters = 'abcdefghijklmnopqrstuvwxyz';
+		let item = Math.floor(Math.random()*letters.length);
+		return letters[item];
+	}
 
-  onAddCharacter(){
-    this.valide = false;
-    if(this.character.length !== 1){
-      return
-    }
-    this.valide = true;
-    console.log(this.character);
+	onGenerateArr() {
+		this.generate_arr.map(
+			(val, index) => {
+				val.map(
+				(value, indx) => {
+					this.generate_arr[index][indx-1] = this.onGenerateCharacter();
+				}
+				)
+			}
+		);
+	}
 
-    // let coint: number = 0;
-    // let arr_row = [];
-    // this.generate_arr.map(
-    //   (val)=>{
-    //     console.log(val);
-    //   }
-    // )
-    // // for(let i = 0; i < 10; i++){
-    // //   arr_row.push(this.character);
-    // // }
-    // // for(let i = 0; i < 10; i++){
-    // //   this.generate_arr.push(arr_row);
-    // // }
-    // console.log(this.generate_arr);
-    // this.finish_generate = true;
+	onGenerateGrid() {
+		this.generate_arr.map(
+			(val, index) => {
+				val.map(
+				(value, indx) => {
+					this.generate_arr[index][indx-1] = this.onGenerateCharacter();
+				}
+				)
+			}
+		);
+		let count_first_character: number = 0;
+		let count_second_character: number = 0;
+		let time = moment().format('ss');
+		let first_character : string = this.generate_arr[time[0]][time[1]];
+		let second_character : string = this.generate_arr[time[1]][time[0]];
+		console.log(`time: "${time}" first_character: ${first_character} second_character: ${second_character}`);
+		this.generate_arr.map(
+			(val, index) => {
+				val.map(
+				(value, indx) => {
+					if (first_character === this.generate_arr[index][indx]) {
+					count_first_character = count_first_character + 1;
+					}
+					if (second_character === this.generate_arr[index][indx]) {
+					count_second_character = count_second_character + 1;
+					}
+				}
+				)
+			}
+		);
+		console.log(`time: "${time}" count_first_character: ${count_first_character} count_second_character: ${count_second_character}`);
+		this.onGenerateCode(count_first_character, count_second_character);
+	}
 
-    this.generate_arr = [
-      ['s', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
-      ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'z']
-    ];
-  }
+	onGenerateCode(first: number, scond: number) {
+		if (first > 9) {
+			let n = Math.ceil(first/9)
+			this.code = Math.ceil(first / n).toString();
+		} else {
+			this.code = first.toString();
+		}
+		if (scond > 9) {
+			let n = Math.ceil(scond/9)
+			this.code += Math.ceil(scond / n).toString();
+		} else {
+			this.code += scond.toString();
+		}
+	}
 
+	onAddCharacter(){
+		this.valide = false;
+		if(this.character.length !== 1){
+		return
+		}
+		this.valide = true;
+		console.log(this.character);
+		let stop: number = 20;
+		let coint: number = 0;
+		this.fixed = true;
+		setTimeout(() => { this.fixed = false; }, 4000); 
+		this.generate_arr.map(
+			(val, index) => {
+				val.map(
+					(value, indx) => {
+						coint = coint + 1;
+						if (coint > 20) {
+							this.generate_arr[index][indx - 1] = this.onGenerateCharacter();
+							return
+						}
+						this.generate_arr[index][indx - 1] = this.character;
+					}
+				)
+			}
+		);
+		let count_first_character: number = 0;
+		let count_second_character: number = 0;
+		let time = moment().format('ss');
+		let first_character : string = this.generate_arr[time[0]][time[1]];
+		let second_character : string = this.generate_arr[time[1]][time[0]];
+		console.log(`time: "${time}" first_character: ${first_character} second_character: ${second_character}`);
+		this.generate_arr.map(
+			(val, index) => {
+				val.map(
+				(value, indx) => {
+					if (first_character === this.generate_arr[index][indx]) {
+					count_first_character = count_first_character + 1;
+					}
+					if (second_character === this.generate_arr[index][indx]) {
+					count_second_character = count_second_character + 1;
+					}
+				}
+				)
+			}
+		);
+		console.log(`time: "${time}" count_first_character: ${count_first_character} count_second_character: ${count_second_character}`);
+		this.onGenerateCode(count_first_character, count_second_character);
+	}
 }
